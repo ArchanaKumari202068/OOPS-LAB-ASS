06e07c50a6db69fb76483c0da850f8313cfd6cda
Archana Kumari <91273746+ArchanaKumari202068@users.noreply.github.com>
2022-03-04 22:37:01 +0530

    Update Program4.cpp
     Write a program to show that the effect of default arguments can alternatively achieved by overloading

--- Program4.cpp
+++ Program4.cpp
@@ -1,16 +1,15 @@
-// Write a program to show that the effect of default arguments can alternatively achievied
-//  by overloading
+// Write a program to show that the effect of default arguments can alternatively achieved by overloading
 #include <iostream>
 using namespace std;
 
-class Mymath{
+class Maths{
   private:
   
   public:
   double power(double m,int n);
   double power(double m);
 };
-double Mymath::power(double m, int n){
+double Maths::power(double m, int n){
     int result = 1;
     for(int i = 0;i<n;i++){
         result *= m;
@@ -18,20 +17,20 @@ double Mymath::power(double m, int n){
     return result;
 }
 
-double Mymath::power(double m){
+double Maths::power(double m){
     int n = 2;
     return power(m,n);
 }
 
 int main()
 {
-    Mymath m1;
+    Maths m1;
     int base,exponent;
-    cout<<"Called power function with single argument\n";
+    cout<<"power function with only base value\n";
     cout<<"Enter base value: ";
     cin>>base;
     cout<<"Value: "<<m1.power(base);
-    cout<<"\nCalled power function with two arguments\n";
+    cout<<"\npower function with both base and exponent value\n";
     cout<<"Enter base value: ";
     cin>>base;
     cout<<"Enter exponent value: ";
