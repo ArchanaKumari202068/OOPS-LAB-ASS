7852ba407cd8d12a387d79890cfc8ac25af8f18a
Archana Kumari <91273746+ArchanaKumari202068@users.noreply.github.com>
2022-03-04 22:14:37 +0530

    Update Program3.cpp
    Write a function power() which raise a number m to a power n. The function takes double value of m and integer value of n and returns the result.Use a default value of n is 2 to make the function to calculate squares when this argument is omitted.

--- Program3.cpp
+++ Program3.cpp
@@ -1,12 +1,12 @@
-
-// Write a function power() which raise a number m to a power n.The function takes double value of m and integer
-// value of n and returns the result.Use a default value of n is 2 to make the function to calculate squares
+// Write a function power() which raise a number m to a power n.The function takes double 
+//value of m and integer value of n and returns the result.
+//Use a default value of n is 2 to make the function to calculate squares
 //  when this argument is omitted. 
 
 #include <iostream>
 
 using namespace std;
-class Math{
+class square{
   public:
       int power(double m,int n = 2){
           double result = 1;
@@ -18,10 +18,10 @@ class Math{
 };
 int main()
 {
-    Math m1;
-    cout<<"Called power function with single argument\n";
+    square m1;
+    cout<<"Result without passing exponent is \n";
     cout<<m1.power(5);
-    cout<<"\nCalled power function with two arguments\n";
+    cout<<"\n Result is\n";
     cout<<m1.power(5,4);
     
     return 0;
