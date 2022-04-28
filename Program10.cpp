b491ede70fadf688543c75df9928f265ecd633e2
Archana <91273746+ArchanaKumari202068@users.noreply.github.com>
2022-04-28 08:59:55 +0530

    Program10.cpp

--- Program10.cpp
+++ Program10.cpp
@@ -17,24 +17,30 @@ class function
    { 
        cout<<endl<<"This shows the deafult constructor."<<endl;
    }
-   function(int X, int Y)                          //parametrised constructor of two arguments
+   function(int X, int Y)                          
+    //parametrised constructor of two arguments
    {
        x=X; 
        y=Y;
-       cout<<"This is parametrised construction with value of a="<<x<<" and b="<<y<<endl;
+       cout<<"This is parametrised construction with value of 
+        a="<<x<<" and b="<<y<<endl;
    }
-   function(function &z)                           //copy constructor
+   function(function &z)                          
+    //copy constructor
    {
        x=z.x;
        y=z.y;
-       cout<<"This is copy constructor :"<<x<<" "<<y<<endl;
+       cout<<"This is copy constructor :
+        "<<x<<" "<<y<<endl;
    }
- function(int p, int q,int r)                          //parametrised constructor of two arguments
+ function(int p, int q,int r)                         
+  //parametrised constructor of two arguments
    {
        x=p; 
        y=q;
        z=r;
-       cout<<"This is constructor overloading with value of a:"<<x<<" and b:"<<y<<" and z: "<<z<<endl;
+       cout<<"This is constructor overloading with value of a:
+        "<<x<<" and b:"<<y<<" and z: "<<z<<endl;
    }
 
 };
