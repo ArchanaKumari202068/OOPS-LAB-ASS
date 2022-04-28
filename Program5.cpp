02d19b9a14259312788c3f7df06cf19b0f70dccd
Archana <91273746+ArchanaKumari202068@users.noreply.github.com>
2022-04-28 09:24:19 +0530

    Program5.cpp

--- Program5.cpp
+++ Program5.cpp
@@ -1,25 +1,32 @@
-// Write a class ACCOUNT that represents your bank account and then use it. The class should allow you to deposit money, withdraw money, calculate intrest
- //and send you a message if you have insufficient balance
+// Write a class ACCOUNT that represents your bank account 
+//and then use it. The class should allow you to 
+//deposit money, withdraw money, calculate intrest
+//and send you a message if you have insufficient balance.
 
   #include <iostream>
 using namespace std;
-
+//class named Account is created
 class Account
-{
-    private:                                    
+{// Access modifier
+    private:
+ //data member
     int bal=0;                                    
-    public:        
+    public: 
+ //Constructor is created
     Account (int a)
     {
         bal=a;
     }
+ //Function is created for deposit money
     int depositeMoney(int add_amount)                 
     {
         bal=bal+add_amount;  
         return bal;
     }
+ //Function is created for withdraw money
     int withdrawMoney(int withdraw_money)                
     {
+     //Conditions
         if(bal>=withdraw_money)
         {
            bal=bal-withdraw_money; 
@@ -30,12 +37,14 @@ class Account
             cout<<"ALERT! Insufficient balance."<<endl;
             return -1;
         }
-    }    
+    }  
+ //To calculate interest
     int interest(int rate, int time)            
     {
        bal=(bal*rate*time/100)+bal;
         return bal;
     }
+ //display curr balance
     void display()                                  
     {
         cout<<"The current balance in your account : "<<bal;
@@ -44,8 +53,11 @@ class Account
 
 int main()
 {
+ //Constructor called
     Account A1=Account(5000);
-    A1.depositeMoney(2000);                       
+ //2000 Money deposited
+    A1.depositeMoney(2000);  
+ //3000 Money withdraw
     int status = A1.withdrawMoney(3000);          
     if(status==1)
     {
